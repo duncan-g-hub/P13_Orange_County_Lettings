29a54fe30ac85ac63228de23110924dde19655ae
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-07-07 11:03:02 +0200

    remove urls names for urls apps

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,7 @@ from oc_lettings_site import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', include("lettings.urls"), name='lettings_index'),
-    path('profiles/', include("profiles.urls"), name='profiles_index'),
+    path('lettings/', include("lettings.urls")),
+    path('profiles/', include("profiles.urls")),
     path('admin/', admin.site.urls),
 ]
