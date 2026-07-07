bc360ecebc5ae8bec033ae75ddcc38e21b544d63
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-07-07 10:33:43 +0200

    manage views in new applications

--- lettings/views.py
+++ lettings/views.py
@@ -4,7 +4,7 @@ from lettings.models import Letting
 # Create your views here.
 # Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat massa. Integer est nunc, pulvinar a
 # tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras eget scelerisque
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'index.html', context)
