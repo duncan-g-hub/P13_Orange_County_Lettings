7eaf205c6979aade284f75940d63d5bd24c6245f
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-07-08 10:34:47 +0200

    manage pluralization for profile, address, letting

--- lettings/models.py
+++ lettings/models.py
@@ -11,6 +11,10 @@ class Address(models.Model):
     zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
+    class Meta:
+        verbose_name = "address"
+        verbose_name_plural = "addresses"
+
     def __str__(self):
         return f'{self.number} {self.street}'
 
@@ -18,6 +22,10 @@ class Address(models.Model):
 class Letting(models.Model):
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "letting"
+        verbose_name_plural = "lettings"
 
     def __str__(self):
         return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -8,5 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
+    class Meta:
+        verbose_name = "profile"
+        verbose_name_plural = "profiles"
+
     def __str__(self):
         return self.user.username
