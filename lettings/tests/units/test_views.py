290a8bab2cdbc14298fa5210e83335db495b3fa7
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-07-09 09:50:52 +0200

    update lettings test views

---
+++ lettings/tests/units/test_views.py
@@ -0,0 +1,27 @@
+from django.urls import reverse
+import pytest
+from pytest_django.asserts import assertTemplateUsed
+
+
+
+@pytest.mark.django_db
+def test_index_view(client, letting_test):
+    response = client.get(reverse('lettings_index'))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'lettings/index.html')
+    assert letting_test in response.context["lettings_list"]
+
+
+@pytest.mark.django_db
+def test_letting_view(client, letting_test):
+    response = client.get(reverse("letting", args=[letting_test.id]))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'lettings/letting.html')
+    assert response.context["title"] == letting_test.title
+    assert (response.context["address"] == letting_test.address)
+
+
+@pytest.mark.django_db
+def test_letting_view_returns_404(client):
+    response = client.get("/lettings/9999/")
+    assert response.status_code == 404
