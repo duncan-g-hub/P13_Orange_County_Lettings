3725a23d20361b7c95c20afe24a9ceb3e051d35b
duncan-g-hub <duncan.gaurat@outlook.fr>
2026-07-23 09:10:21 +0200

    manage debug and allowed_hosts with .env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,9 +19,9 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG").lower() == "true"
+DEBUG = os.getenv("DEBUG", True).lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 # Sentry
 sentry_sdk.init(
